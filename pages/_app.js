c4dc933954bb7799e18b8e06a6c1191b1a2c0fca
atharva <misalatharva407@gmail.com>
2024-10-19 13:31:27 +0530

    change favicon

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
 import "@/styles/globals.css";
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+
+  return (
+    <>
+      <Head>
+        <title>Jeevan Hotel</title>
+        <link rel="icon" href="/JEEVAN-LOGO-brown.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
